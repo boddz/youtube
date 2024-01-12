17c45f8b594c9c237c6570fc8c36288e8a93ad7c
boddz <boddz.dev@gmail.com>
2024-01-12 00:09:31 +0000

    Magic comment/ shebang added to tests

--- tests/test_all.rb
+++ tests/test_all.rb
@@ -1,6 +1,10 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
 require 'test/unit'
 
 require_relative 'test_extractor'
 require_relative 'test_thumbnail'
+require_relative 'test_video'
 
 #https://ruby-doc.org/stdlib-3.0.2/libdoc/test-unit/rdoc/Test/Unit/Assertions.html
